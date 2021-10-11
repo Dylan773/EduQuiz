23389e065e2393f3a008b5820072c2e28a064b25
Dylan Brand <dylanbrand773@gmail.com>
2021-10-11 17:31:28 +0100

    Initial Commit

---
+++ desktop/src/com/dylan773/finalyearproject/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.dylan773.finalyearproject.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.dylan773.finalyearproject.MyGdxGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new MyGdxGame(), config);
+	}
+}
