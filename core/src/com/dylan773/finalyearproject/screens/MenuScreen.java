eb8b72c7bf12120393396c7f03a04e0f24eb1857
Dylan Brand <dylanbrand773@gmail.com>
2021-10-20 17:36:03 +0100

    First Design Main Menu and Options Screen
    * First implementation of the main menu and options screen for this application.
    * Commented out OptionScreen class for development.

--- core/src/com/dylan773/finalyearproject/screens/MenuScreen.java
+++ core/src/com/dylan773/finalyearproject/screens/MenuScreen.java
@@ -1,12 +1,16 @@
 package com.dylan773.finalyearproject.screens;
 
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Input;
 import com.badlogic.gdx.ScreenAdapter;
+import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.g2d.TextureRegion;
+import com.badlogic.gdx.scenes.scene2d.Actor;
 import com.badlogic.gdx.scenes.scene2d.InputEvent;
 import com.badlogic.gdx.scenes.scene2d.Stage;
 import com.badlogic.gdx.scenes.scene2d.ui.*;
+import com.badlogic.gdx.scenes.scene2d.utils.ChangeListener;
 import com.badlogic.gdx.scenes.scene2d.utils.ClickListener;
 import com.badlogic.gdx.scenes.scene2d.utils.TextureRegionDrawable;
 import com.dylan773.finalyearproject.Assets;
@@ -19,14 +23,15 @@ import com.dylan773.finalyearproject.EducationGame;
 //TODO - Change title font and colour
 public class MenuScreen extends ScreenAdapter {
 
+
     // Fields
     private Stage stage;
     private Table table;
     private final EducationGame game;
+    private OptionWindow optionWindow = new OptionWindow("", Assets.SKIN);
 
     // Constructor
     /**
-     *
      * @param game
      */
     public MenuScreen(EducationGame game) {
@@ -37,6 +42,7 @@ public class MenuScreen extends ScreenAdapter {
 
         stage = new Stage();
         stage.addActor(table);
+        stage.addActor(optionWindow);
         Gdx.input.setInputProcessor(stage);
 
         // TODO - debug
@@ -55,7 +61,7 @@ public class MenuScreen extends ScreenAdapter {
         Label gameLabel = new Label("Edu Quiz", Assets.SKIN, "title");
         table.add(gameLabel).pad(0, 0, 20, 0).row();
 
-
+        // Play Game Button
         addButton("Play Game").addListener(new ClickListener() {
 //            @Override
 //            public void clicked(InputEvent event, float x, float y)
@@ -64,25 +70,25 @@ public class MenuScreen extends ScreenAdapter {
 //            }
         });
 
+        // Option Window Button
         addButton("Options").addListener(new ClickListener() {
             @Override
-            public void clicked(InputEvent event, float x, float y)
-            {
-                game.setScreen(new OptionsScreen(game));
+            public void clicked(InputEvent event, float x, float y) {
+                //game.setScreen(new OptionsScreen(game));
+                optionWindow.setVisible(true);
             }
         });
 
+        // Exit Game Button
         addButton("Exit Game").addListener(new ClickListener() {
             @Override
-            public void clicked(InputEvent event, float x, float y)
-            {
+            public void clicked(InputEvent event, float x, float y) {
                 Gdx.app.exit();
             }
         });
     }
 
     /**
-     *
      * @param name
      * @return
      */
@@ -90,6 +96,7 @@ public class MenuScreen extends ScreenAdapter {
         TextButton button = new TextButton(name, Assets.SKIN);
         table.add(button).width(400f).padBottom(20f).row();
 
+        //TODO - button sound
         return button;
     }
 
@@ -102,6 +109,12 @@ public class MenuScreen extends ScreenAdapter {
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT); // clears the screen so it can draw from fresh
         stage.act(Gdx.graphics.getDeltaTime()); // act - tells the ui to perfrom actions (checks for inputs)
         stage.draw();
+
+        // Checks for keyboard input if the options window is currently visible on the stage.
+        if (optionWindow.isVisible()) {
+            if (Gdx.input.isKeyPressed(Input.Keys.ESCAPE))
+                optionWindow.setVisible(false);
+        }
 
 //        Gdx.gl.glClearColor(0f, 0f, 0f, 1);
 //        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
@@ -117,5 +130,57 @@ public class MenuScreen extends ScreenAdapter {
     @Override
     public void dispose() {
         //stage.dispose();
+    }
+
+    /**
+     *
+     */
+    private static class OptionWindow extends Window { //TODO - Set the values of the sliders and checkbox to the same value as the audio, so they dont reset back to 0 each time
+        public OptionWindow(String title, Skin skin) {
+            super(title, skin);
+            this.setResizable(false);
+            this.setMovable(false);
+            this.setVisible(false); // Initially the window is hidden
+            this.setSize(1000f, 600f);
+            //this.top();
+            this.setPosition(150, 50); // TODO - I dont like this hard coded, center using a different method
+
+            Label titleLabel = new Label("OPTIONS", Assets.SKIN, "title");
+            Label audioLabel = new Label("Audio", Assets.SKIN, "subtitle");
+            Label musicLabel = new Label("Music Volume:", Assets.SKIN);
+            Label sfxLabel = new Label("SFX Volume:", Assets.SKIN);
+            Label menuExit = new Label("Press ESC to Exit", Assets.SKIN, "font", Color.ORANGE);
+
+
+            // Game Music Control
+            Slider musicSlider = new Slider(0.0f, 1.0f, 0.1f, false, Assets.SKIN);
+            musicSlider.addListener(new ChangeListener() {
+                @Override
+                public void changed(ChangeEvent event, Actor actor) {
+
+                }
+            });
+
+            // Game Sound Effect's Control
+            Slider sfxSlider = new Slider(0.0f, 1.0f, 0.1f, false, Assets.SKIN);
+            sfxSlider.addListener(new ChangeListener() {
+                @Override
+                public void changed(ChangeEvent event, Actor actor) {
+
+                }
+            });
+
+            // Master Audio Mute
+            CheckBox muteCheck = new CheckBox("Mute", Assets.SKIN);
+
+            // Add each actor to the window
+            this.add(titleLabel).colspan(2).row();
+            this.add(audioLabel).padBottom(10f).colspan(2).row();
+            this.add(musicLabel).padBottom(5f);
+            this.add(musicSlider).padBottom(5f).row();
+            this.add(sfxLabel, sfxSlider).row();
+            this.add(muteCheck).padBottom(157f).colspan(2).row();
+            this.add(menuExit).colspan(2);
+        }
     }
 }
